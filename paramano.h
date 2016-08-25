e9966ba3c07365baf6848a395bc52d4c95d66331
David Phillips <dbphillipsnz@gmail.com>
2016-08-25 22:09:03 +1200

    Move license to central file, add acknowledgement to trayfreq

--- paramano.h
+++ paramano.h
@@ -1,19 +1,1 @@
-/************************************************************************
- * This file is part of Paramano.                                       *
- *                                                                      *
- * Paramano is free software; you can redistribute it and/or            *
- * modify it under the terms of the GNU General Public License as       *
- * published by the Free Software Foundation; either version 3 of the   *
- * License, or (at your option) any later version.                      *
- *                                                                      *
- * Paramano is distributed in the hope that it will be useful,          *
- * but WITHOUT ANY WARRANTY; without even the implied warranty of       *
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        *
- * GNU General Public License for more details.                         *
- *                                                                      *
- * You should have received a copy of the GNU General Public License    *
- * along with Paramano. If not, see                                     *
- * <http://www.gnu.org/licenses/>.                                      *
- ************************************************************************/
-
 void config_init();
